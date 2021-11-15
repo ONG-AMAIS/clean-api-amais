684fca63ebc2f50fb8656341e3a18463d89cfa1a
Lucas Santos <lucas.santos@pagtel.com.br>
2021-11-15 22:31:44 +0000

    chore: add __init__.py in modules

--- amais/infra/db/helpers/config.py
+++ amais/infra/db/helpers/config.py
@@ -3,10 +3,10 @@ from amais.main.configs.constants import DATABASE
 from flask_sqlalchemy import SQLAlchemy
 
 
-connection_string = 'mysql+pymysql://{user}:{password}@{host}:{port}/{db_name}'.format(
+CONNECTION_STRING = 'mysql+pymysql://{user}:{password}@{host}:{port}/{db_name}'.format(
     user=DATABASE['user'], password=DATABASE['password'], host=DATABASE['host'], port=DATABASE['port'], db_name=DATABASE['db_name'])
 
-app.config['SQLALCHEMY_DATABASE_URI'] = connection_string
+app.config['SQLALCHEMY_DATABASE_URI'] = CONNECTION_STRING
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
