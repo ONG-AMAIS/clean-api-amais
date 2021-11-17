5c7a111e4ae8a978b75b6fd4c549598a1e6fd23a
Lucas Santos <lucassm02@gmail.com>
2021-11-17 05:09:55 +0000

    feat: add generic list of users

--- amais/infra/db/user/user_repository.py
+++ amais/infra/db/user/user_repository.py
@@ -2,6 +2,8 @@ from typing import Dict
 from sqlalchemy.sql import func
 from ..helpers.config import db
 from .user_entity import User
+from ..person.person_entity import Person
+from ..user_type.user_type_entity import UserType
 
 
 class UserRepository():
@@ -23,37 +25,54 @@ class UserRepository():
 
     @ classmethod
     def get_all(cls):
-        result = User.query.all()
+        result = db.session.query(Person.name, Person.cpf, UserType.name, User.created_at).select_from(
+            User).join(Person, Person.person_id == User.person_id).join(UserType, UserType.user_type_id == User.user_type_id).all()
 
-        return (({'id': row.user_id, 'login': row.login, 'password': row.password}) for row in result)
+        return cls.__format_generic_user(result)
 
     @ classmethod
     def find_by_id(cls, id: int):
-        row = User.query.filter_by(user_id=id).first()
-        return {'id': row.user_id, 'login': row.login, 'password': row.password}
+        result = User.query.filter_by(user_id=id).first()
+        return cls.__format_user(result)
 
     @ classmethod
     def update_by_id(cls, id: int, login: str, password: str):
-        row = User.query.filter_by(user_id=id).first()
+        result = User.query.filter_by(user_id=id).first()
 
-        if not row:
+        if not result:
             return None
 
-        row.login = login
-        row.password = password
-        row.updated_at = func.now()
+        result.login = login
+        result.password = password
+        result.updated_at = func.now()
 
         db.session.commit()
 
-        return {'id': row.user_id, 'login': row.login, 'password': row.password}
+        return cls.__format_user(result)
 
     @ classmethod
     def delete_by_id(cls, id: int):
-        row = User.query.filter_by(user_id=id).first()
+        result = User.query.filter_by(user_id=id).first()
 
-        if not row:
+        if not result:
             return None
 
-        row.deleted_at = func.now()
+        result.deleted_at = func.now()
 
         db.session.commit()
+
+    @ classmethod
+    def __format_generic_user(cls, user: User) -> dict:
+
+        if isinstance(user, list):
+            return (({'name': item.name, 'document': item.cpf,
+                      'type': item[2], 'created_at': str(item.created_at)}) for item in user)
+
+        return {'name': user.name, 'document': user.cpf, 'type': user[2], 'created_at': str(user.created_at)}
+
+    @ classmethod
+    def __format_user(cls, user: User) -> dict:
+        if isinstance(user, list):
+            return (({'id': item.user_id, 'login': item.login}) for item in user)
+
+        return {'id': user.user_id, 'login': user.login}
