dbe1ceede1db790b2be6e66e5b5c4ac899e7f117
Lucas Santos <lucassm02@gmail.com>
2021-11-18 02:05:31 +0000

    chore: add new variables to contants

--- amais/main/configs/constants.py
+++ amais/main/configs/constants.py
@@ -2,6 +2,9 @@ from dotenv import dotenv_values
 
 config = dotenv_values('.env')
 
+PORT = int(config['PORT']) or 5000
+
+ALLOW_DEBUG = config['FLASK_ENV'] == 'development'
 
 DATABASE = {'user': config['DB_USER'],
             'password': config['DB_PASSWORD'],
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from amais.main.server import app
+from amais.main.configs.constants import ALLOW_DEBUG, PORT
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=PORT, debug=ALLOW_DEBUG)
