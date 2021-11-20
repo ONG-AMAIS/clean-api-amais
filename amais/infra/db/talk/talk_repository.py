4ece3f9edbe9c09dad06408c3624ed42c1983771
Lucas Santos <lucassm02@gmail.com>
2021-11-20 13:42:57 +0000

    chore: add talk list

--- amais/infra/db/talk/talk_repository.py
+++ amais/infra/db/talk/talk_repository.py
@@ -19,6 +19,14 @@ class TalkRepository():
         talk = Talk.query.filter_by(person_id=person_id).first()
         return cls.__format_voluntary(talk)
 
+    @ classmethod
+    def get_all(cls):
+        talk = Talk.query.all()
+        return cls.__format_voluntary(talk)
+
     @classmethod
     def __format_voluntary(cls, talk: Talk) -> dict:
-        return {'id': talk.talk_id, 'person_id': talk.person_id}
+        if isinstance(talk, list):
+            return (({'id': item.item_id, 'title': item.title, 'date': item.date, 'created_at': item.created_at}) for item in talk)
+
+        return {'id': talk.talk_id, 'title': talk.title, 'date': talk.date, 'created_at': talk.created_at}
